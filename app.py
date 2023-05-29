22c4dec1f5a9e1b6aa3ed2a762f62e224ad3877d
Sandeep Pandey <sapandey@altimetrik.com>
2023-05-29 14:47:40 +0530

    Appended exception

--- app.py
+++ app.py
@@ -1,21 +1,24 @@
 from flask import Flask, request, jsonify
-import numpy as np
+import random
 
 app = Flask(__name__)
 
-
-@app.route('/api/random_array', methods=['POST'])
+@app.route('/random_array', methods=['POST'])
 def random_array():
-    sentence = request.json['sentence']
+    try:
+        sentence = request.json.get('sentence')
+        if not sentence:
+            return jsonify({'error': 'Missing sentence parameter'}), 400
 
-    # Tokenize the sentence into words
-    words = sentence.split()
+        array = generate_random_array()
+        return jsonify(array)
 
-    # Generate a random 500-dimensional array of floats
-    random_array = np.random.rand(500).tolist()
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
 
-    return jsonify(random_array)
-
+def generate_random_array():
+    array = [random.uniform(0, 1) for _ in range(500)]
+    return array
 
 if __name__ == '__main__':
     app.run()
